a4e00ddfc6d5971e231e59c397f20d001bf1da88
alfredasare <alfredasare101@gmail.com>
2021-04-12 21:54:02 +0000

    Start channel and episode redux preparations

--- pages/podcasts/index.js
+++ pages/podcasts/index.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import PodcastRowList from '@/components/podcast-row-list/podcastRowListComponent';
 import Search from '@/components/search/searchComponent';
 
-const Podcasts = () => {
+import { setCurrentPodcast } from '../../lib/redux/player/player.actions';
+import { selectCurrentPodcast } from '../../lib/redux/player/player.selectors';
+
+const Podcasts = ({ currentPodcast, setCurrentPodcast }) => {
+	console.log(currentPodcast);
+
+	useEffect(() => {
+		setCurrentPodcast({
+			name: 'Alfred',
+		});
+	}, []);
+
 	return (
 		<>
 			<Search />
@@ -10,4 +25,12 @@ const Podcasts = () => {
 	);
 };
 
-export default Podcasts;
+const mapStateToProps = createStructuredSelector({
+	currentPodcast: selectCurrentPodcast,
+});
+
+const mapDispatchToProps = dispatch => ({
+	setCurrentPodcast: podcast => dispatch(setCurrentPodcast(podcast)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Podcasts);
